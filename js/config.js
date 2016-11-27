0dd3b54c9ea878b5de9c41a32417bff9faaff4c9
Eduardo <eduardok.fx@gmail.com>
2016-11-27 18:49:27 -0200

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,14 @@
+
+function setConfig(){
+    
+    /*	
+	    var texts = {
+	        "title":"Controle de Produtos"
+	    };
+	    document.title = texts.title;
+	    document.getElementById("navTitle").innerHTML = texts.title;
+    */
+    document.getElementById("navTitle").innerHTML = "Controle de Produtos";
+}
+
+setConfig();
